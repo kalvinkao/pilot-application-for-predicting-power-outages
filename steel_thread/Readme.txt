3aa25384078a5a125f724dfa40a19bcf3192e2e2
muthderd <kalvin.d.kao@gmail.com>
2017-11-20 16:10:41 -0800

    start of steel thread

---
+++ steel_thread/Readme.txt
@@ -0,0 +1,12 @@
+after cloning, copy the steel_thread folder to your working directory
+cd into the copy of the steel_thread folder
+bash remove_headers.sh to format the historical weather and outage CSVs
+bash setup_virtual_environment.sh installs Anaconda to /data and starts the virtual environment for the steel thread
+
+do a clean startup of hadoop, postgres, and hive metastore:
+1) mount HDFS volume
+2) /root/start-hadoop.sh
+3) /data/start_postgres.sh
+4) /data/start_metastore.sh
+
+then run spark-submit run_steel_thread.py
---
+++ steel_thread/run_steel_thread.py
@@ -0,0 +1,36 @@
+from pyspark.sql import SQLContext
+from pyspark.sql.types import *
+import steel_thread
+
+sqlContext = SQLContext(sc)
+
+outageData=sc.textFile("file:///home/w205/steel_thread/outage_history.csv")
+weatherData=sc.textFile("file:///home/w205/steel_thread/weather_history.csv")
+
+riOutages = outageData.filter(lambda x: "Rhode Island" in x)
+riOutageRecords = riOutages.map(lambda r : r.split(","))
+weatherRecords = weatherData.map(lambda r : r.split(","))
+
+RI_Outages = riOutageRecords.map(lambda p: (p[2],p[4],p[7],p[10]))
+#delimiters are all fucked up for the outage data
+RI_Weather = weatherRecords.map(lambda p: (p[5],p[10],p[16], p[17], p[18], p[19]))
+
+outageSchemaString = 'DATETIME DURATION AREA NUMCUSTOMERS'
+weatherSchemaString = 'DATETIME DRYBULBTEMP HUMIDITY WINDSPEED WINDDIRECTION WINDGUSTSPEED'
+
+outageFields = [StructField(field_name, StringType(), True) for field_name in outageSchemaString.split()]
+weatherFields = [StructField(field_name, StringType(), True) for field_name in weatherSchemaString.split()]
+
+outageSchema = StructType(outageFields)
+weatherSchema = StructType(weatherFields)
+
+schemaOutageData = sqlContext.createDataFrame(RI_Outages, outageSchema)
+schemaWeatherData = sqlContext.createDataFrame(RI_Weather, weatherSchema)
+
+schemaOutageData.registerTempTable('RI_Outages')
+schemaWeatherData.registerTempTable('RI_Weather')
+
+results = sqlContext.sql('SELECT * FROM RI_Weather LIMIT 10')
+results.show()
+
+steel_thread.random_prediction()
