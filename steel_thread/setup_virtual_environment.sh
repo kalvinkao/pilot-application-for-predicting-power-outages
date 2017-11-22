8623a02bd398611036a2200f6f7d479005c6398a
muthderd <kalvin.d.kao@gmail.com>
2017-11-21 20:03:05 -0800

    correction to setup_virtual_environment.sh

--- steel_thread/setup_virtual_environment.sh
+++ steel_thread/setup_virtual_environment.sh
@@ -7,7 +7,7 @@ cd /data
 wget https://repo.continuum.io/archive/Anaconda3-5.0.1-Linux-x86_64.sh
 bash Anaconda3-5.0.1-Linux-x86_64.sh
 conda create -n steel_thread numpy scipy scikit-learn pandas
-activate steel_thread
+source activate steel_thread
 
 # change directory back to the original
 cd $MY_CWD
