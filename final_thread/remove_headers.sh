7a72e84fd43b094a30098453d956f018b9036d89
joanna408 <joanna.huang@ischool.berkeley.edu>
2017-12-16 21:00:45 -0800

    Update with new Boston csv name

--- final_thread/remove_headers.sh
+++ final_thread/remove_headers.sh
@@ -6,15 +6,11 @@
 # remove first line of files and rename
 
 OLD_FILE="1130585.csv"
-NEW_FILE="weather_history.csv"
-tail -n +2 "$OLD_FILE" > $NEW_FILE
-
-OLD_FILE="1154416.csv"
-NEW_FILE="weather_history_Massachusetts.csv"
-tail -n +2 "$OLD_FILE" > $NEW_FILE
-
-OLD_FILE="1154447.csv"
 NEW_FILE="weather_history_Rhode_Island.csv"
+tail -n +2 "$OLD_FILE" > $NEW_FILE
+
+OLD_FILE="1154483.csv"
+NEW_FILE="weather_history_Massachusetts.csv"
 tail -n +2 "$OLD_FILE" > $NEW_FILE
 
 OLD_FILE="Table_B_1.csv"
