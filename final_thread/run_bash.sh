a47490e6ea21abf08b872ddc8c48dd0eb56d3cd9
joanna408 <joanna.huang@ischool.berkeley.edu>
2017-12-19 13:42:38 -0800

    Updating directory path

--- final_thread/run_bash.sh
+++ final_thread/run_bash.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 PATH=/data/anaconda3/envs/steel_thread/bin:/data/spark15/bin:/data/anaconda3/bin:/usr/local/bin:/bin:/usr/bin:/usr/local/sbin:/usr/sbin:/sbin:/opt/jdk1.7.0_79/bin:/usr/lib/spark/bin:/usr/lib/hadoop/bin:/home/w205/bin
-cd /home/w205/w205_final_project/final_thread
+cd /home/w205/final_thread
 source activate steel_thread
 /data/spark15/bin/spark-submit run_final_thread_v1.py >> results.txt
 source deactivate
